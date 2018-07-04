9e374fe7c06f44ea2784489646d0fae654f1be27
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-04 11:53:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 
 def display_board(gamedata)
-  puts(" #{gamedata[0]} |  #{gamedata[1]} |  #{gamedata[2]} ")
+  puts(" #{gamedata[0]} | #{gamedata[1]} | #{gamedata[2]} ")
   puts("-----------")
-  puts(" #{gamedata[3]} |  #{gamedata[4]} |  #{gamedata[5]} ")
+  puts(" #{gamedata[3]} | #{gamedata[4]} | #{gamedata[5]} ")
   puts("-----------")
-  puts(" #{gamedata[6]} |  #{gamedata[7]} |  #{gamedata[8]} ")
+  puts(" #{gamedata[6]} | #{gamedata[7]} | #{gamedata[8]} ")
 end
